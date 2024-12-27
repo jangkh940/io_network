072e9189db19a65f53e57d3751ddadbeae3d297e
장규훈 <kh.jang@okestro.com>
2024-12-27 14:16:52 +0900

    채팅 프로그램 version2 작업 - 각 명령어별 기능구현

--- src/network/chat/server/Session.java
+++ src/network/chat/server/Session.java
@@ -62,10 +62,10 @@ public class Session implements Runnable {
   }
 
   public String getUsername() {
-    return this.getUsername();
+    return this.username;
   }
 
-  private void setUsername(String username) {
+  public void setUsername(String username) {
     this.username = username;
   }
 }
